ecefadb07287aeb0e0a02fe096331f86ea139add
Jen Williams <jen@swirrl.com>
2020-03-25 14:35:09 +0000

    PMD validation - 1 code list per dimension

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -3,11 +3,18 @@
 
 PROJ_DIR=/Users/jenw/Documents/GitHub/ons-data-export
 CUBE_VALS_DIR=${PROJ_DIR}/validations/cube
+PMD_VALS_DIR=${PROJ_DIR}/validations/pmd
 
-echo "Running RDF Cube validations..."
+
 
 # The Stardog SPARQL endpoint is http://<server>:<port>/{db}/query
 
-CMD_CUBE="java -jar ${PROJ_DIR}/validations/rdf-validator-0.3.0-SNAPSHOT-standalone.jar --endpoint http://localhost:5820/gss_export/query --suite ${CUBE_VALS_DIR}"
-echo $CMD_CUBE
-eval $CMD_CUBE
+# echo "Running RDF Cube validations..."
+# CMD_CUBE="java -jar ${PROJ_DIR}/validations/rdf-validator-0.3.0-SNAPSHOT-standalone.jar --endpoint http://localhost:5820/gss_export/query --suite ${CUBE_VALS_DIR}"
+# echo $CMD_CUBE
+# eval $CMD_CUBE
+
+echo "Running PMD validations..."
+CMD_PMD="java -jar ${PROJ_DIR}/validations/rdf-validator-0.3.0-SNAPSHOT-standalone.jar --endpoint http://localhost:5820/gss_export/query --suite ${PMD_VALS_DIR}"
+echo $CMD_PMD
+eval $CMD_PMD
