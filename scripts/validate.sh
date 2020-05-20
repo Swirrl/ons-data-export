3b626268aba5f221e5310e71a5e723bf5d048b57
Jen Williams <jen@swirrl.com>
2020-05-20 16:59:38 +0100

    Adjust validation script to run against a specific set of dataset graphs one at a time (based on the graph URIs given in a CSV file as a param)

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
+# set -e #exit on error
 
+VALIDATOR=$1
+DB_QE=$2
+TEST_SUITE=$3
+GRAPH_FILE=$4
 
-PROJ_DIR=/Users/jenw/Documents/GitHub/ons-data-export
-CUBE_VALS_DIR=${PROJ_DIR}/validations/cube
-PMD_VALS_DIR=${PROJ_DIR}/validations/pmd
+if [[ -n "$VALIDATOR" && 
+		-n "$DB_QE" && 
+	  -n "$TEST_SUITE" &&
+	-n "$GRAPH_FILE" ]]; then
 
+    
+    echo "Looking up graph details from $GRAPH_FILE"
+    echo
 
+    OLDIFS=$IFS
+    IFS=','
+    [ ! -f $GRAPH_FILE ] && { echo "$GRAPH_FILE file not found"; exit 99; }
+    while read graph_uri
+    do
+      # echo "Graph : $graph_uri"
+      # printf '%q\n' "$graph_uri"
+      CLEAN_GRAPH=${graph_uri//$'\r'/}
+      
+      if [[ $CLEAN_GRAPH != "graph_uri" ]] 
+      then
+      	echo
+      	echo "PROCESSING VALIDATIONS FOR $CLEAN_GRAPH"
 
-# The Stardog SPARQL endpoint is http://<server>:<port>/{db}/query
+        VALIDATE_CMD="java -jar ${VALIDATOR} --endpoint ${DB_QE} --suite ${TEST_SUITE} --graph ${CLEAN_GRAPH}"
 
-# echo "Running RDF Cube validations..."
-# CMD_CUBE="java -jar ${PROJ_DIR}/validations/rdf-validator-0.3.0-SNAPSHOT-standalone.jar --endpoint http://localhost:5820/gss_export/query --suite ${CUBE_VALS_DIR}"
-# echo $CMD_CUBE
-# eval $CMD_CUBE
+        echo $VALIDATE_CMD
+        eval $VALIDATE_CMD
 
-echo "Running PMD validations..."
-CMD_PMD="java -jar ${PROJ_DIR}/validations/rdf-validator-0.3.0-SNAPSHOT-standalone.jar --endpoint http://localhost:5820/gss_export/query --suite ${PMD_VALS_DIR}"
-echo $CMD_PMD
-eval $CMD_PMD
+      fi
+
+    done < $GRAPH_FILE
+    IFS=$OLDIFS
+
+    
+
+  	echo
+  	echo "FINISHED VALIDATING ALL GRAPHS"
+  	echo 
+
+else
+    echo script error: There appears to be an error a required variable was not set.    
+    echo "usage:"
+     echo "validate.sh <validator-jar> <db-query-endpoint> <test-suite> <graphs-file>"
+    echo "e.g. ./validate.sh /path/to/rdf-validator-standalone.jar http://localhost:7820/pmd-data/query /path/to/validations-dir /path/to/graphs.csv"
+fi
